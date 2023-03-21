44e9605b20efceb24bbd6af42b68daab73345b2a
ankitkakodia <126950333+ankitkakodia@users.noreply.github.com>
2023-03-21 22:56:47 +0530

    update db_setup
    insert category names in categories

--- db_setup.py
+++ db_setup.py
@@ -4,27 +4,29 @@ from settings import *
 conn = pymysql.connect(host=host, user=user, password=password,)
 cur = conn.cursor()
 
-print("""
-Welcome to Quote-Hub
-1. login
-2. signup
-""")
-
 cur.execute(f"drop database if exists {database}")
-create_database = f"""create database if not exists {database} ;"""
+print("Database Deleted")
+create_database = f"""create database {database} ;"""
+
 cur.execute(create_database)
+print("Database created")
 
 cur.execute(f"""use {database};""")
+print("use database")
 
-
-
-create = f"create table if not exists users (id int auto_increment primary key, first_name varchar(15) not null, last_name varchar(15) not null, password varchar(128) not null, username varchar(15) unique);"
+create = f"create table users (id int auto_increment primary key, first_name varchar(15) not null, last_name varchar(15) not null, password varchar(128) not null, username varchar(15) unique);"
 cur.execute(create)
-create = f"create table if not exists categories(cat_id int auto_increment primary key, cat_name varchar(50));"
+print("User table created")
+create = f"create table categories(cat_id int auto_increment primary key, cat_name varchar(50));"
 cur.execute(create)
-create = f"create table if not exists quotes (id int auto_increment not null primary key, user_id int, category_id int not null, quotes varchar(255) unique , author varchar(20),foreign key (user_id) references users(id), foreign key (category_id) references categories(cat_id));"
+print("Categories table created")
+create = f"create table quotes (id int auto_increment not null primary key, user_id int, category_id int not null, quotes varchar(255) unique , author varchar(20),foreign key (user_id) references users(id), foreign key (category_id) references categories(cat_id));"
 cur.execute(create)
-
+print("Quotes table created")
+insert = f"insert into categories (cat_name) values ('Motivational'),('Funny'),('Sad'),('Love'),('Friendship'),('Political'),('Others');"
+cur.execute(insert)
+conn.commit()
+print("Insert categories")
 
 
 conn.close()
